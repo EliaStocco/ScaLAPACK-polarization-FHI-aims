9d586bac5ec15b6c62c083d91f1455699ddae566
Elia Stocco <elia.stocco@mpsd.mpg.de>
2026-05-29 11:03:36 +0200

    updated

--- run-all.sh
+++ run-all.sh
@@ -2,4 +2,5 @@ cd validation
 mkdir -p data
 python scripts/check-files.py  -i dataframe.csv 
 python scripts/extract-data.py -i dataframe.csv -o data/dataframe.csv
+python scripts/delta-polarization.py -i data/dataframe.csv -o data/delta-polarization.csv
 cd -
